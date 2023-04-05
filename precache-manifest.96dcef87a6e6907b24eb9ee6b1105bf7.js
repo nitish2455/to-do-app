da66ca54814df1609afe9354d186fd191b71b9cb
nitish2455 <nitish98888@gmail.com>
2023-04-05 13:16:48 +0530

    Updates

---
+++ precache-manifest.96dcef87a6e6907b24eb9ee6b1105bf7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455dfbde35b3f1b1120bbcf1b59f5cf6",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "41f252c523c677377e60",
+    "url": "/to-do-app/static/css/3.bb57abf7.chunk.css"
+  },
+  {
+    "revision": "5119f37625047c7023e5",
+    "url": "/to-do-app/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "8e7df16328f432be1d04",
+    "url": "/to-do-app/static/js/2.69c1aa52.chunk.js"
+  },
+  {
+    "revision": "41f252c523c677377e60",
+    "url": "/to-do-app/static/js/3.c605b0c6.chunk.js"
+  },
+  {
+    "revision": "6ecded4a2681e3fc2f90",
+    "url": "/to-do-app/static/js/4.9863be55.chunk.js"
+  },
+  {
+    "revision": "5119f37625047c7023e5",
+    "url": "/to-do-app/static/js/main.f3742e9b.chunk.js"
+  },
+  {
+    "revision": "8e778484da209713bbce",
+    "url": "/to-do-app/static/js/runtime-main.2d752498.js"
+  }
+]);
